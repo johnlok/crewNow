155f538c0911545ba511f1e14beb94a68c48ae37
johnlok <john.lok.djinn@gmail.com>
2014-10-29 21:27:11 +0800

    crewted tripcrewrelationship table and crewboard form creates new table entry

--- app/controllers/crew_controller.rb
+++ app/controllers/crew_controller.rb
@@ -3,12 +3,26 @@ class CrewController < ApplicationController
 
   def crewsubmit
     @crew_params = params
-    @raceinformation = params[:raceinformation]
+    @racetitle = params[:racetitle]
     name = params[:new_crew_member_name]
     email = params[:new_crew_member_email]
     notifications = params[:new_crew_member_notifications]
     tel = params[:new_crew_member_tel]
     bio = params[:new_crew_member_bio]
-    CrewMember.create(name: name, email: email, tel: tel, bio: bio, notifications: notifications)
+    newcrew = CrewMember.new(name: name, email: email, tel: tel, bio: bio, notifications: notifications)
+    newcrew.save
+    crewid = newcrew.id
+    binding.pry
+    #the below code grabs the selected trips that the user has selected and puts them into an array.
+    @crewtrips = []
+    @crew_params.keys.each do |key|
+      if key.start_with?('select_trip_')
+        @crewtrips << @crew_params[key]
+      end
+    end
+    @crewtrips.each do |trip|
+      relationship = TripCrewRelationship.new(trip_id: trip, crewmember_id: crewid, status: "available")
+      relationship.save
+    end
   end
 end
