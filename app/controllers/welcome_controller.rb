6cb4fccc955c7210658d21d95d40c08cdda50486
johnlok <john.lok.djinn@gmail.com>
2014-10-31 14:42:49 +0800

    testing email

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    # UserMailer.confirmation_email(current_user).deliver
+    UserMailer.confirmation_email(current_user).deliver
   end
 end
