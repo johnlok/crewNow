dbeddc079e698466ebff2985e0136071a23178f2
johnlok <john.lok.djinn@gmail.com>
2014-10-31 13:33:04 +0800

    updated some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Race.create race_date:"2014-10-25 11:00:00", race_name: "Training",  race_description: "Training for Dummies. Steve spills all secrets.", race_location: "Port Shelter"
 Race.create race_date:"2014-11-01 14:00:00", race_name: "Jasmin 2",  race_description: "Jasmin 2", race_location: "Port Shelter"
 Race.create race_date:"2014-11-08 14:00:00", race_name: "Passage Race",  race_description: "Passage Race", race_location: "Port Shelter"
 Race.create race_date:"2014-11-15 14:00:00", race_name: "Lipton Trophy",  race_description: "Lipton Trophy", race_location: "Harbour"
@@ -20,12 +19,21 @@ t2 = Trip.create boat_name: "Ironman", trip_organizer: nil, trip_description: "J
 t3 = Trip.create boat_name: "Fly By Wire", trip_organizer: nil, trip_description: "Jasmin 2", trip_attendees: 3, trip_attendee_limit: "4", race_id: 2
 t4 = Trip.create boat_name: "Phoenix", trip_organizer: nil, trip_description: "Jasmin 2", trip_attendees: 3, trip_attendee_limit: "4", race_id: 2
 
-c1 = CrewMember.create name: 'John', email: 'john@lok.com', tel: 12345678, bio: 'abc'
-c2 = CrewMember.create name: 'Harry', email: 'harry@potter.com', tel: 12345678, bio: 'def'
-c3 = CrewMember.create name: 'Stephen', email: 'stephen@gray.com', tel: 12345678, bio: 'ghi'
-c4 = CrewMember.create name: 'Fiona', email: 'fiona@you.com', tel: 12345678, bio: 'jkl'
-c5 = CrewMember.create name: 'Astrid', email: 'astrid@large.com', tel: 12345678, bio: 'mno'
+c1 = CrewMember.create name: 'John', email: 'john@lok.com', tel: 12345678, bio: 'John the sailor.'
+c2 = CrewMember.create name: 'Harry', email: 'harry@potter.com', tel: 12345678, bio: 'Harry the sailor'
+c3 = CrewMember.create name: 'Stephen', email: 'stephen@gray.com', tel: 12345678, bio: 'Stephen the sailor'
+c4 = CrewMember.create name: 'Fiona', email: 'fiona@you.com', tel: 12345678, bio: 'Fiona the sailor'
+c5 = CrewMember.create name: 'Astrid', email: 'astrid@large.com', tel: 12345678, bio: 'Astrid the sailor'
 
 TripRelationship.create trip: t1, crew_member: c1, status: 'confirmed'
 TripRelationship.create trip: t1, crew_member: c2, status: 'confirmed'
+TripRelationship.create trip: t1, crew_member: c1, status: 'available'
 TripRelationship.create trip: t2, crew_member: c1, status: 'available'
+TripRelationship.create trip: t3, crew_member: c1, status: 'available'
+TripRelationship.create trip: t3, crew_member: c1, status: 'available'
+TripRelationship.create trip: t3, crew_member: c2, status: 'available'
+TripRelationship.create trip: t3, crew_member: c3, status: 'available'
+TripRelationship.create trip: t3, crew_member: c4, status: 'available'
+TripRelationship.create trip: t3, crew_member: c5, status: 'available'
+
+
